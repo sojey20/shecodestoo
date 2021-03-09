44f66ab7b36492004cba264b2697b37346033f25
Patrick Atkinson <pa13269@my.bristol.ac.uk>
2021-03-09 20:58:47 +0000

    add starting js and html for testimonies

---
+++ testimony.js
@@ -0,0 +1,26 @@
+var currentTestimony = 0;
+var numTestimonies = document.getElementsByClassName("testimony").length;
+
+showTestimony(currentTestimony);
+
+function nextTestimony() { 
+  if (++currentTestimony >= numTestimonies) currentTestimony = 0;
+  showTestimony(currentTestimony); 
+}
+
+function prevTestimony() { 
+  if (--currentTestimony < 0) currentTestimony = numTestmonies - 1;
+  showTestimony(currentTestimony); 
+}
+
+function showTestimony(showIndex) {
+  console.log('index = ', showIndex)
+  var slides = document.getElementsByClassName("testimony");
+  if (showIndex >= slides.length) showIndex = 0;
+  if (showIndex < 0) showIndex = slides.length - 1;
+  for (var i = 0; i < slides.length; ++i) {
+    slides[i].style.display = "none";
+  }
+  slides[showIndex].style.display = "block";
+  slides[showIndex].className += " activeTestimony";
+}
