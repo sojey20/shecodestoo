8fd9881ac026ca0097e8adbb4458af35086995ca
raghad <raghad.hav@gmail.com>
2021-05-18 16:06:09 +0000

    add deploy file

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+cp index.html /var/www/shecodestooiq.com/html/
+cp courses.html /var/www/shecodestooiq.com/html/
+cp index.css /var/www/shecodestooiq.com/html/
+cp about.html /var/www/shecodestooiq.com/html/
+cp testimony.js /var/www/shecodestooiq.com/html/
+cp -r img /var/www/shecodestooiq.com/html/
+cp -r icons /var/www/shecodestooiq.com/html/
