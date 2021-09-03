766f1fc13156295f3525068c5dc490cbb858dcc9
Raghad Hav <raghad.hav@gmail.com>
2021-09-03 19:13:55 +0300

    adding more courses and fixing home page

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cp index.html /var/www/shecodestooiq.com/html/
 cp courses.html /var/www/shecodestooiq.com/html/
 cp index.css /var/www/shecodestooiq.com/html/
+cp style.css /var/www/shecodestooiq.com/html/
 cp about.html /var/www/shecodestooiq.com/html/
 cp testimony.js /var/www/shecodestooiq.com/html/
 cp -r img /var/www/shecodestooiq.com/html/
